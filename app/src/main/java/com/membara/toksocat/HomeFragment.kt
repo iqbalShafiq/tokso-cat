a571ab9064f70ed845d992c3917e52ca4c015802
Iqbal Shafiq Rozaan <iqbal.syafiq.rozaan@gmail.com>
2021-11-29 13:18:57 +0700

    initial commit

---
+++ app/src/main/java/com/membara/toksocat/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.membara.toksocat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.membara.toksocat.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/membara/toksocat/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.membara.toksocat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.membara.toksocat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
