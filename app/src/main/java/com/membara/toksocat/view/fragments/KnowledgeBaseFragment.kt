3618baaefc93daa8de0b45ec62866383d119f5c3
Iqbal Shafiq Rozaan <iqbal.syafiq.rozaan@gmail.com>
2022-01-04 23:55:29 +0700

    Add UI to ResultFragment

---
+++ app/src/main/java/com/membara/toksocat/view/fragments/KnowledgeBaseFragment.kt
@@ -0,0 +1,29 @@
+package com.membara.toksocat.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.membara.toksocat.databinding.FragmentKnowledgeBaseBinding
+
+class KnowledgeBaseFragment : Fragment() {
+
+    private lateinit var binding: FragmentKnowledgeBaseBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentKnowledgeBaseBinding.inflate(
+            inflater,
+            container,
+            false
+        );
+
+
+
+        return binding.root
+    }
+}
